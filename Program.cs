6e65a10371b56e1c8a0aaf04298d542347ccf407
hwong23 <hwong23@gmail.com>
2023-11-18 17:24:29 +0000

    upd

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@ var app = builder.Build();
 
 app.Urls.Add("http://localhost:5000");
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => "Hello HWO World!");
 
 app.MapGet("/{cityName}/weather", GetWeatherByCity);
 
